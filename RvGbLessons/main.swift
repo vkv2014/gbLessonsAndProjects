2d9994ff3992054019cb782fc9ffa0b353765bdc
Roman Vinokurov <roman.vinokurov@onoffapp.com>
2021-09-12 20:20:37 +0300

    initial commit

--- RvGbLessons/main.swift
+++ RvGbLessons/main.swift
@@ -8,4 +8,15 @@
 import Foundation
 
 print("Hello, World!")
+let number3: Float = 12.2
 
+print(number3)
+
+let isTrue = true
+if isTrue {
+    print("true")
+} else {
+    print ("false")
+}
+var temp = 10
+print("\(temp)  he,ktq")
